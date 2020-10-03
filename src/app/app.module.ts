72fd4f7e95e2d30be5f796ec632bb8486b814690
piotreqd <piotrduda919@gmail.com>
2020-10-03 22:05:48 +0200

    Chart components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    BarChartComponent,
+    PieChartComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule
