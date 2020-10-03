76f1ca883f9af3285affd71f335021932ca3e441
piotreqd <piotrduda919@gmail.com>
2020-10-03 23:45:47 +0200

    Setup routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from '../routes';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -7,6 +9,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +20,14 @@ import { LineChartComponent } from './charts/line-chart/line-chart.component';
     SidebarComponent,
     BarChartComponent,
     PieChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
