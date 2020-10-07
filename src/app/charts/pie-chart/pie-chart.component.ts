aa6cb980797c20eedde686f39fb47c660fa46372
piotreqd <piotrduda919@gmail.com>
2020-10-08 00:22:56 +0200

    .

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,15 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [150, 450, 120];
+  pieChartLabels: string[] = ['lol', 'hehe', 'trolololo'];
+  colors: any[] = [
+    {
+       backgroundColor: ['#26547C', '#FF6B6B', '#FFD166']
+    }
+  ];
+  pieChartType: 'pie';
+
   ngOnInit(): void {
   }
 
