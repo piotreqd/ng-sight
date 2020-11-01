5d7fa5dba2c0881872ebe2807c396aafeafcaea4
piotreqd <piotrduda919@gmail.com>
2020-11-01 14:50:43 +0100

    mock table data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/Order';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {
+      id: 1,
+      total: 230,
+      customer: {id: 1, name: 'AIUT sp. z.o.o', state: 'Śląskie', email: 'cos@gmail.com'},
+      placed: new Date(2020, 11, 1),
+      fulfilled : new Date(2020, 11, 20),
+      status: 'Completed'
+    }
+  ];
 
   ngOnInit(): void {
   }
